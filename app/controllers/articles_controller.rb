4315a5e12d28d3c9304368f514ab46934da9dced
Narayanan V <narayananv@Mac-D-Narayanan.local>
2018-12-04 19:42:23 +0530

    Sample blog changes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+
+	def new
+		@article = Article.new
+	end
+
+    def create
+    	@article = Article.new(params.require(:article).permit(:title,:author,:text))
+    	binding.pry
+    	if @article.save
+    		redirect_to @article
+  		else
+  			render :action => 'new'
+  		end
+    end
+
+    def show
+    	@article = Article.find(params[:id])
+    end
+
+    def index
+    	@article = Article.all.order(title: :asc)
+    end
+
+    def edit
+    	@article = Article.find(params[:id])
+    end
+
+    def update
+    	@article = Article.find(params[:id])
+
+    	if @article.update_attributes(params.require(:article).permit(:title,:text))
+    		@article.save
+    		redirect_to :action => 'show' , :id => @article
+    	else
+    		render :action => 'edit'
+    	end
+    end
+
+    def destroy
+    	@article = Article.find(params[:id])
+    	@article.destroy
+    	redirect_to articles_path
+    end
+
+end
